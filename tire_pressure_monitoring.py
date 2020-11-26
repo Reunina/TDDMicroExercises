65ec1fb48d6dd867999f7fa094d0bc023ec3dcdb
sara <sara.tahabik@gmail.com>
2020-11-26 18:07:17 +0100

    Correcting lint issues

--- tire_pressure_monitoring.py
+++ tire_pressure_monitoring.py
@@ -1,12 +1,14 @@
 import random
 
+
 class Sensor(object):
 
-    # The reading of the pressure value from the sensor is simulated in this implementation.
+    # The reading of the pressure value from the sensor
+    # is simulated in this implementation.
     # Because the focus of the exercise is on the other class.
 
     _OFFSET = 16
-        
+
     def pop_next_pressure_psi_value(self):
         pressure_telemetry_value = self.sample_pressure()
         return Sensor._OFFSET + pressure_telemetry_value
@@ -19,16 +21,20 @@ class Sensor(object):
 
 
 class Alarm(object):
-
+    """
+    Alarm that goes on when presure is not on the expected range
+    needs a Sensor
+    """
     def __init__(self):
         self._low_pressure_threshold = 17.0
         self._high_pressure_threshold = 21.0
         self._sensor = Sensor()
         self._is_alarm_on = False
-        
+
     def check(self):
         psi_pressure_value = self._sensor.pop_next_pressure_psi_value()
-        if psi_pressure_value < self._low_pressure_threshold or self._high_pressure_threshold < psi_pressure_value:
+        if (psi_pressure_value < self._low_pressure_threshold or
+                self._high_pressure_threshold < psi_pressure_value):
             self._is_alarm_on = True
 
     @property
